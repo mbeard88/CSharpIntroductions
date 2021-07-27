e5ef7de360f256201e762df0bfaa4e21acb463be
Martin <mbeard@spartaglobal.com>
2021-07-27 12:31:49 +0100

    End of CSharpIntroduction

--- CSharpIntroductions/CSharpIntroductions/Program.cs
+++ CSharpIntroductions/CSharpIntroductions/Program.cs
@@ -6,7 +6,20 @@ namespace CSharpIntroductions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 100;
+            x += 10;
+            for (int i=0; i<= 10; i++)
+            {
+                Console.WriteLine(i);
+                x += i;
+            }
+
+            Console.WriteLine("Writing out command line arguments");
+            foreach(string item in args)
+            {
+                Console.WriteLine(item);
+            }
+            
         }
     }
 }
